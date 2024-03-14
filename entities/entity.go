579b4f53f6f5fc678521fe4a8aebf92ad54329e3
ohmspeed777 <aomspeed222@gmail.com>
2024-03-14 18:37:39 +0700

    update config

--- entities/entity.go
+++ entities/entity.go
@@ -4,5 +4,5 @@ import "github.com/google/uuid"
 
 type Image struct {
 	ID          uuid.UUID `gorm:"primaryKey;autoIncrement;type:uuid" json:"id"`
-	ImageBase64 string    `json:"iamgeBase64" validate:"required"`
+	ImageBase64 string    `json:"imageBase64" validate:"required"`
 }
