b2749a7316426db4a7df699425625652acf4ba66
DESKTOP-4GPR761\HashTable <Rayato159@gmail.com>
2024-03-18 18:39:55 +0700

    :package: fix inserting data

--- server/dataInserting.go
+++ server/dataInserting.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"context"
-	"encoding/base64"
 	"encoding/json"
 	"time"
 
@@ -13,33 +12,15 @@ import (
 
 func (s *echoServer) DataInserting(c echo.Context) error {
 	ctx := context.Background()
-	key := c.Param("key")
-
-	fileHeader, err := c.FormFile("file")
-	if err != nil {
-		s.logger.Error("get file error: ", err.Error())
-		return err
-	}
-
-	file, err := fileHeader.Open()
-	if err != nil {
-		s.logger.Error("get file error: ", err.Error())
-		return err
-	}
-
-	defer file.Close()
-
-	var data []byte
-	_, err = file.Read(data)
-	if err != nil {
-		s.logger.Error("get file error: ", err.Error())
-		return err
-	}
 
 	reqImage := new(entities.Image)
+	if err := c.Bind(reqImage); err != nil {
+		s.logger.Error("Error binding request: ", err.Error())
+		return c.String(500, err.Error())
+	}
+
 	uuidV7, _ := uuid.NewV7()
 	reqImage.ID = uuidV7.String()
-	reqImage.ImageBase64 = base64.StdEncoding.EncodeToString(data)
 
 	reqImageJson, err := json.Marshal(reqImage)
 	if err != nil {
@@ -47,7 +28,7 @@ func (s *echoServer) DataInserting(c echo.Context) error {
 		return err
 	}
 
-	if result := s.rdb.Set(ctx, key, string(reqImageJson), 5*time.Minute); result.Err() != nil {
+	if result := s.rdb.Set(ctx, reqImage.ID, string(reqImageJson), 5*time.Minute); result.Err() != nil {
 		s.logger.Errorf("Error setting cache: %s", result.Err().Error())
 		return result.Err()
 	}
@@ -57,5 +38,5 @@ func (s *echoServer) DataInserting(c echo.Context) error {
 		return err
 	}
 
-	return c.JSON(200, reqImage)
+	return c.JSON(201, reqImage)
 }
